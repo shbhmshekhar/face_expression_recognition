296298e3db7f097d3fab840ae0a698300f6d8f3c
Shubham Shekhar <shbhmshekhar@outlook.com>
2021-07-09 11:04:57 +0530

    final commit to master

---
+++ main.js
@@ -0,0 +1,36 @@
+//import faceapi from 'face-api'
+const video = document.getElementById("video");
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('models')
+]).then(startVideo)
+
+function startVideo(){
+    navigator.getUserMedia(
+        {video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+//startVideo();
+
+video.addEventListener('play',()=>{
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = {width: video.width, height: video.height};
+    faceapi.matchDimensions(canvas, displaySize);
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        //console.log(detections);
+        const resizeDetection = faceapi.resizeResults(detections,displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizeDetection)
+        faceapi.draw.drawFaceLandmarks(canvas, resizeDetection);
+        faceapi.draw.drawFaceExpressions(canvas, resizeDetection);
+    }, 100)
+	console.log('connected')
+})
